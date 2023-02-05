4b2fd52d674d1594acda089220485f67f155ca52
n-ruzhik <anastasiyaruzhetskaya@gmail.com>
2023-02-05 15:33:37 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const button = document.querySelector(".change-color");
+const currentColor = document.querySelector(".color");
+
+button.addEventListener("click", onChangeBgColor);
+
+function onChangeBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  currentColor.textContent = document.body.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
